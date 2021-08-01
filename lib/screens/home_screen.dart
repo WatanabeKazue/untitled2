c12eaa55bc71a57790b985eb7d81b422d9e5d559
WatanabeKazue <kazueWatanabe.202105@gmail.com>
2021-08-01 09:44:53 +0900

    これで完成ですか。

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,12 +10,19 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  String _text01 = "おめでとうございます";
-  String _text02 = "合格です";
-  String _text03 = "よくできました";
-  String _text04 = "残念でした";
-  String _text05 = "不合格です";
-  String _text06 = "頑張りましょう";
+  String _textHeader = "あなたの名前を教えてください。";
+  TextEditingController _textEditingController = TextEditingController();
+  Widget _imageWidget = Container();
+
+  //String _text01 = "おめでとうございます";
+  //String _text02 = "合格です";
+  //String _text03 = "よくできました";
+  //String _text04 = "残念でした";
+  //String _text05 = "不合格です";
+  //String _text06 = "頑張りましょう";
+
+  List<int> _soundIds = [0,0,0,0,0,0];
+  //late Soundpool _soundpool;
 
   List<String> _texts = ["おめでとうございます","合格です","よくできました","残念でした","不合格です","頑張りましょう"];
 
@@ -57,6 +64,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: <Widget>[
                   Expanded(flex: 1, child: _soundButton(_texts[4])),  //TODO ボタン
                   Expanded(flex: 1, child: _soundButton(_texts[5])), //TODO ボタン
+
+
+
+
                 ],
               ),
             ),
@@ -66,13 +77,16 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  widget _soundButton() {
+
+  //TODO 戻り値の型を大文字から+引数の設定要
+  Widget _soundButton(String displayText) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
         onPressed: null,
-        child: (""),
+        //TODO 引数として渡された文字をTextウィジェットで表示
+        child: Text(""),
       ),
     );
   }
-}
+}//TODO 98
