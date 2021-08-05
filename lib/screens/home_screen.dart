f52335dbcee2f666997215ce0615bcdf55a19db1
keybow <keybow007@gmail.com>
2021-08-05 16:32:45 +0900

    必要な修正＋やるべきことの記述

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,25 @@
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+/*
+* TODO[やるべきこと]
+*  １）sound_poolパッケージのインストール（レクチャー102）
+*   https://school.minpro.net/courses/670160/lectures/11951485
+*
+*   ２）Soundpoolの初期化と破棄の処理（レクチャー104）
+*   https://school.minpro.net/courses/670160/lectures/11951488
+*
+*   ３）loadSoundメソッドの作成から続きをやる（レクチャー111の5:55から）
+*   https://school.minpro.net/courses/670160/lectures/11951494
+* */
 
-  String _textHeader = "あなたの名前を教えてください。";
-  TextEditingController _textEditingController = TextEditingController();
-  Widget _imageWidget = Container();
+
+class _HomeScreenState extends State<HomeScreen> {
+  //TODO[やったこと] はじめてのアプリのコードが入っていたので削除
 
   //String _text01 = "おめでとうございます";
   //String _text02 = "合格です";
@@ -20,34 +28,37 @@ class _HomeScreenState extends State<HomeScreen> {
   //String _text05 = "不合格です";
   //String _text06 = "頑張りましょう";
 
-  List<int> _soundIds = [0,0,0,0,0,0];
+  List<int> _soundIds = [0, 0, 0, 0, 0, 0];
+
   //late Soundpool _soundpool;
   int number = 3;
 
-  List<String> _texts = ["おめでとうございます","合格です","よくできました","残念でした","不合格です","頑張りましょう"];
+  List<String> _texts = [
+    "おめでとうございます",
+    "合格です",
+    "よくできました",
+    "残念でした",
+    "不合格です",
+    "頑張りましょう"
+  ];
 
   @override
   void initState() {
     super.initState();
+    //TODO[やったこと] _initSoundsメソッドが消えているのでニョロニョロが出ていた
     _initSounds();
-  print("initState終わったで～=buildメソッド回ったで～");
+    print("initState終わったで～=buildメソッド回ったで～");
   }
- // }
 
-  //Future<void>
+  // }
 
+  //TODO[やったこと] initSoundsメソッドが消えているので復活
+  Future<void> _initSounds() async {
+    _soundIds[0] = await loadSound("assets/sounds/sound1.mp3");
 
- // _soundIds[0] = await loadSound("assets/sounds/sound1.mp3");
-  //_soundIds[1] = await loadSound("assets/sounds/sound1.mp3");
- // _soundIds[2] = await loadSound("assets/sounds/sound1.mp3");
-//  _soundIds[3] = await loadSound("assets/sounds/sound1.mp3");
- // _soundIds[4] = await loadSound("assets/sounds/sound1.mp3");
- // _soundIds[5] = await loadSound("assets/sounds/sound1.mp3");
-
-  print("initState終わったで～=効果音ロードできたで～");
-  @override
-  void setState() {
-    super.setState();
+    //TODO[やったこと] このprint関数とsetStateメソッドは_initSoundsメソッド内に要る必要あり
+    print("initState終わったで～=効果音ロードできたで～");
+    setState(() {});
   }
 
   @override
@@ -67,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   Expanded(flex: 1, child: _soundButton(_texts[0])), //TODO ボタン
-                  Expanded(flex: 1, child: _soundButton(_texts[1])),  //TODO ボタン
+                  Expanded(flex: 1, child: _soundButton(_texts[1])), //TODO ボタン
                 ],
               ),
             ),
@@ -76,8 +87,8 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Expanded(flex: 1, child: _soundButton(_texts[2])),  //TODO ボタン
-                  Expanded(flex: 1, child: _soundButton(_texts[3])),  //TODO ボタン
+                  Expanded(flex: 1, child: _soundButton(_texts[2])), //TODO ボタン
+                  Expanded(flex: 1, child: _soundButton(_texts[3])), //TODO ボタン
                 ],
               ),
             ),
@@ -86,12 +97,8 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Expanded(flex: 1, child: _soundButton(_texts[4])),  //TODO ボタン
+                  Expanded(flex: 1, child: _soundButton(_texts[4])), //TODO ボタン
                   Expanded(flex: 1, child: _soundButton(_texts[5])), //TODO ボタン
-
-
-
-
                 ],
               ),
             ),
@@ -101,15 +108,14 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-
-  //TODO 戻り値の型を大文字から+引数の設定要
+  //戻り値の型を大文字から+引数の設定要
   Widget _soundButton(String displayText) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
         onPressed: null,
-          child: Text(displayText),
+        child: Text(displayText),
       ),
     );
   }
-}//TODO 98
+} //TODO 98
